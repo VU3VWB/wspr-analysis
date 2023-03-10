08bd0154ac20c040e5799900f084026130863729
VU3VWB <vu3vwb@gmail.com>
2023-03-10 13:57:05 +0800

    Add a plotutils plot command to the script. The plot is junk though

--- parse_script_example.sh
+++ parse_script_example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat data_feb2023_vk6.csv | grep VK6CQ | grep VK5ARG | awk -F ',' '{print $17 }' 
+cat data_feb2023_vk6.csv | grep VK6CQ | grep VK5ARG | awk -F ',' '{print $17 }' | graph -T X 
