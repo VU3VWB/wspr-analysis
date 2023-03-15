336ece422b471f1c14891eed4e350dcb3aa3d75b
VU3VWB <vu3vwb@gmail.com>
2023-03-15 14:14:57 +0800

    modified

--- analyse_2023_02.sh
+++ analyse_2023_02.sh
@@ -2,4 +2,4 @@
 
 rm -f VK5ARG_VK6CQ-2023-02.csv
 awk -F ',' '$3 == "VK5ARG" {print $2, $5}' <  VK6CQ-2023-02.csv >> VK5ARG_VK6CQ-2023-02.csv
-
+graph -T X --bitmap-size "1920x1080" < VK5ARG_VK6CQ-2023-02.csv
